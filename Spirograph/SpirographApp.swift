65d33ea3eb8a17262f9f34d772dfa246a7ebda38
rafaellevy <srlevy2@gmail.com>
2020-12-02 17:04:41 -0800

    Initial commit

---
+++ Spirograph/SpirographApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpirographApp.swift
+//  Spirograph
+//
+//  Created by Rafael-Levy  on 12/2/20.
+//
+
+import SwiftUI
+
+@main
+struct SpirographApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
